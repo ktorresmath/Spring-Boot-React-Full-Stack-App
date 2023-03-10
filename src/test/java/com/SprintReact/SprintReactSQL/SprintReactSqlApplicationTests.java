70c61be81300444f2ff3db96e365e832b344368a
kelvin <kelvinthusky@gmail.com>
2023-03-10 02:46:16 -0500

    first change

---
+++ src/test/java/com/SprintReact/SprintReactSQL/SprintReactSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SprintReact.SprintReactSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintReactSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
